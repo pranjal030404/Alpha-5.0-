0ef14ab8448531d0bc69db87102409e3bcaff18c
pranjal030404 <pranjalp160@gmail.com>
2025-05-17 21:08:06 +0530

    table for any no given by user

---
+++ Calculate_the_table_of_a_no.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Calculate_the_table_of_a_no {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the no whose table you want: ");
+        int a = sc.nextInt();
+        System.out.println("Your table is :");
+        System.out.println(a * 1);
+        System.out.println(a * 2);
+        System.out.println(a * 3);
+        System.out.println(a * 4);
+        System.out.println(a * 5);
+        System.out.println(a * 6);
+        System.out.println(a * 7);
+        System.out.println(a * 8);
+        System.out.println(a * 9);
+        System.out.println(a * 10);
+    }
+}
