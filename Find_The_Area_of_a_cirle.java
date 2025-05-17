228deedc1ce26eda094e0da886c69d8c67c195d5
pranjal030404 <pranjalp160@gmail.com>
2025-05-17 20:52:44 +0530

    Area of circle calculated

---
+++ Find_The_Area_of_a_cirle.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class Find_The_Area_of_a_cirle {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.err.println("Enter the radius of the circle: ");
+        float a = sc.nextInt();
+        float area = a * 3.14f;
+        System.out.println("The area of a circle whose radius is " + a + "and area is " + area);
+    }
+}
